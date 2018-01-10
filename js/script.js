4dc52c0c1d0c932e6945967af7e050af32e547ca
jarekpod <34914355+jarekpod@users.noreply.github.com>
2018-01-10 14:51:07 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+yeah, very funny task
+*/
+var name = prompt('Enter your name');
+//my first comment, 'alert' it is a very cool thing
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+//end of my boring comment
